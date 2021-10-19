0297fe6de816ba6fe6e77999ebbe8a7151277ac4
floriansaubin <saubinflorian@gmail.com>
2021-10-19 20:36:40 +0200

    setup fonts, colors-variables & other stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import Header from "./components/Header";
 import './stylesheets/portfolio.scss';
 
 function App() {
   return (
     <div className="portfolio">
+      <ul className="menu">
+        <li className="menu-item">HOME</li>
+        <li className="menu-item">WORK</li>
+        <li className="menu-item">CONTACT</li>
+      </ul>
+      <main id="main">
+        <Header />
+      </main>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import "../stylesheets/header.scss";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <a href="www.floriansaubin.com" className="header-title">FLORIAN</a>
+      <p className="header-sub-title">FRONT-END Developer</p>
+    </div>
+  )
+}
