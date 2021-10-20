57a0991c2aec19161914b44c5a9827d5f2cebfd0
floriansaubin <saubinflorian@gmail.com>
2021-10-20 09:16:21 +0200

    prep routing pages

---
+++ src/pages/BlogNode.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BlogNode() {
+  return (
+    <div className="blog-node">
+      
+    </div>
+  )
+}
---
+++ src/pages/WidgetService.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function WidgetService() {
+  return (
+    <div className="widget-service">
+      
+    </div>
+  )
+}
