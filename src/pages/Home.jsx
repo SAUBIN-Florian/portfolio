0bb6fd98f64d3335fa13ce751e8be3fc1313de61
floriansaubin <saubinflorian@gmail.com>
2021-10-21 13:36:04 +0200

    adding global structure of showcase projects page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,20 @@ export default function Home() {
   return (
     <div className="home-page">
         <div className="hero-banner">
+          <div className="hero-banner-content">
+            <h1 className="hero-banner-title">WELCOME TO MY PORTFOLIO !</h1>
+            <p className="hero-banner-para">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+              Deleniti tempora deserunt harum provident consectetur iste, 
+              quidem unde officia architecto illo dolorum ea alias laudantium beatae. 
+              Eum vel voluptate repellat nihil!
+              Repellat nesciunt illum facere, eligendi voluptate ex eius maxime animi. 
+              Exercitationem, aliquid dicta eum blanditiis fugiat, 
+              quidem at non cum animi ratione amet quo voluptatem cumque. 
+              Hic pariatur quibusdam ratione!
+            </p>
+          </div>
+          <img className="hero-banner-img" src="https://via.placeholder.com/500/D1462F/FFFFFF?Text=PlaceholderC/O https://placeholder.com/" alt="placeholder" />
         </div>
         <div className="carousel">
           <Link to="/landing-page">
--- src/pages/WidgetService.jsx
+++ src/pages/WidgetService.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import "../stylesheets/projectspage.scss";
+import { Link } from "react-router-dom";
 
 export default function WidgetService() {
   return (
     <div className="widget-service">
-      widget
+      <div className="page-title">
+        <h3 className="title-date">2021</h3>
+        <h1 className="title-large">WIDGET SERVICE</h1>
+      </div>
+      <div className="page-section-content">
+        <div className="page-scope">
+          <h3>SCOPE</h3>
+          <ul>
+            <li>point 1</li>
+            <li>point 2</li>
+            <li>point 3</li>
+          </ul>
+        </div>
+        <div className="page-summary">
+          <h3>SUMMARY</h3>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Velit ad consequatur porro necessitatibus quae, 
+            voluptates minus aut sapiente illo, doloribus ducimus ex. 
+            Delectus, consectetur laborum maxime natus numquam ipsa rerum!</p>
+            <a href="https://saubin-florian.github.io/landing-page-demo/" target="blank">Go to site...</a>
+            <a href="https://github.com/SAUBIN-Florian/landing-page-demo" target="blank">Check the code...</a>
+        </div>
+      </div>
+      <div className="page-section-video">
+        <video src="">Placeholder</video>
+      </div>
+      <div className="page-section-img">
+        <img src="https://via.placeholder.com/150/FFFF00/000000C/O https://placeholder.com/" alt="placeholder" />
+        <img src="https://via.placeholder.com/150/FFFF00/000000C/O https://placeholder.com/" alt="placeholder" />
+        <img src="https://via.placeholder.com/150/FFFF00/000000C/O https://placeholder.com/" alt="placeholder" />
+      </div>
+      <div className="page-section-link">
+        <Link to="/blog-node">Blog Node</Link>
+        <Link to="/landing-page">Landing Page</Link>
+      </div>
     </div>
   )
 }
