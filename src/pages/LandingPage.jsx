33991c9a64cce0a33de46def73f4702b159649f7
floriansaubin <saubinflorian@gmail.com>
2021-10-25 10:12:58 +0200

    some img advencement

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -12,17 +12,17 @@ export default function LandingPage() {
         <div className="page-scope">
           <h3>SCOPE</h3>
           <ul>
-            <li>point 1</li>
-            <li>point 2</li>
-            <li>point 3</li>
+            <li>Vanilla Javascript</li>
+            <li>Smooth transitions</li>
+            <li>Typical landing page style</li>
           </ul>
         </div>
         <div className="page-summary">
           <h3>SUMMARY</h3>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Velit ad consequatur porro necessitatibus quae, 
-            voluptates minus aut sapiente illo, doloribus ducimus ex. 
-            Delectus, consectetur laborum maxime natus numquam ipsa rerum!</p>
+            <p>
+              This is a french website about an event compagny specialized in wedding, karaoke and many more.
+              For this website i was focus on the smoothness of animations through the page, like scroll, link buttons...
+            </p>
             <a href="https://saubin-florian.github.io/landing-page-demo/" target="blank">Go to site...</a>
             <a href="https://github.com/SAUBIN-Florian/landing-page-demo" target="blank">Check the code...</a>
         </div>
@@ -31,14 +31,12 @@ export default function LandingPage() {
         <video autoPlay muted loop>
           <source src="/videos/landing-cut.mp4" type="video/mp4" />
         </video>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil magni quasi sed, 
-          officia eveniet veritatis fuga ut commodi, 
-          beatae nesciunt ipsam dolore laborum voluptatem et dicta velit, 
-          minima ratione doloremque!</p>
+        <p>Fully responsive design for all devices, desktop, tablet, phone...</p>
       </div>
       <div className="page-section-img">
-        <img src="/images/mockup-landing.png" alt="mockup of the pc demo" />
-        <img src="/images/mockup-responsive-landing.png" alt="mockup of the phone demo" />
+        <img src="/images/landing-section.jpg" alt="mockup of the pc demo" />
+        <img src="/images/landing-responsive.jpg" alt="mockup of the tablet demo" />
+        <img src="/images/landing-responsive-2.jpg" alt="mockup of the phone demo" />
       </div>
       <div className="page-section-link">
         <Link to="/widget-service" className="previous-link">Widget Service</Link>
