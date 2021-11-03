5816e0a0278fdfa6cae1ae168c889d247837c0a7
floriansaubin <saubinflorian@gmail.com>
2021-11-03 18:15:42 +0100

    installing three.js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,23 @@
+import { useEffect } from "react";
 import Slider from "../components/Slider";
 import "../stylesheets/home.scss";
 
 export default function Home() {
+
+  useEffect(()=>{
+    
+  }, []);
+
   return (
     <div className="home-page">
         <div className="hero-banner">
           <div className="hero-banner-content">
-            <h1 className="hero-banner-title">WELCOME TO MY PORTFOLIO !</h1>
-            <p className="hero-banner-para">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-              Deleniti tempora deserunt harum provident consectetur iste, 
-              quidem unde officia architecto illo dolorum ea alias laudantium beatae. 
-              Eum vel voluptate repellat nihil!
-              Repellat nesciunt illum facere, eligendi voluptate ex eius maxime animi. 
-              Exercitationem, aliquid dicta eum blanditiis fugiat, 
-              quidem at non cum animi ratione amet quo voluptatem cumque. 
-              Hic pariatur quibusdam ratione!
-            </p>
+            <h1 className="hero-banner-title">Hello, I'm <span className="hero-banner-span">Florian SAUBIN</span> !</h1>
+            <p className="hero-banner-para">I'm a Front-End Developer from Lyon, FR.</p>
+            <p className="hero-banner-para">I have passion for UI effects, logic and creating intuitive user experiences.</p>
+            <p className="hero-banner-para">I like to keep in touch with Back-End technologies too !</p>
           </div>
-          <img className="hero-banner-img" src="https://via.placeholder.com/500/D1462F/FFFFFF?Text=PlaceholderC/O https://placeholder.com/" alt="placeholder" />
+          <canvas id="canvas"></canvas>
         </div>
         <Slider />
     </div>
