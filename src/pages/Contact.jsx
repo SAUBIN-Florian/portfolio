abd9b6e96eeb9d53231fc68de9f6b6cf3dd40cd0
floriansaubin <saubinflorian@gmail.com>
2021-12-01 12:41:56 +0100

    email.js config + dotenv

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,32 @@
+import emailjs from "emailjs-com";
 import "../stylesheets/contact.scss";
 
+
 export default function Contact() {
+
+  const {REACT_APP_SERVICE_ID, REACT_APP_TEMPLATE_ID, REACT_APP_USER_ID} = process.env;
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(REACT_APP_SERVICE_ID, REACT_APP_TEMPLATE_ID, e.target, REACT_APP_USER_ID)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  }
+
   return (
     <div className="contact-page">
-      <form className="contact-form" action="POST">
+      <form className="contact-form" onSubmit={sendEmail}>
         <label htmlFor="">Name:</label>
-        <input type="text" placeholder="Enter name..." />
+        <input type="text" name="name" placeholder="Enter name..." />
         <label htmlFor="">Email:</label>
-        <input type="text" placeholder="Enter email..." />
+        <input type="text" name="email" placeholder="Enter email..." />
         <label htmlFor="">Message:</label>
-        <textarea className="textarea" placeholder="I would love to work with you..." />
+        <textarea className="textarea" name="message" placeholder="I would love to work with you..." />
         <input className="contact-btn" type="submit" value="SUBMIT" />
       </form>
     </div>
