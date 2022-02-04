054b0ad2b9e6fc429e797cca4e04674454731e7d
floriansaubin <saubinflorian@gmail.com>
2022-02-04 17:08:11 +0100

    go back to work on my portfolio

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -14,25 +14,32 @@ export default function Contact() {
 
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs.sendForm(REACT_APP_SERVICE_ID, REACT_APP_TEMPLATE_ID, e.target, REACT_APP_USER_ID)
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
+    if(data.name && data.email && data.message){
+      emailjs.sendForm(REACT_APP_SERVICE_ID, REACT_APP_TEMPLATE_ID, e.target, REACT_APP_USER_ID)
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
       e.target.reset();
+    }else{
+      console.log("Completez tous les champs !")
+    }
+  }
+
+  const onChange = (e) => {
+    setData({...data, [e.target.name]: e.target.value})
   }
 
   return (
     <div className="contact-page">
       <form className="contact-form" onSubmit={sendEmail}>
-        <label htmlFor="">Name:</label>
-        <input type="text" name="name" placeholder="Enter name..." />
-        <label htmlFor="">Email:</label>
-        <input type="text" name="email" placeholder="Enter email..." />
-        <label htmlFor="">Message:</label>
-        <textarea className="textarea" name="message" placeholder="I would love to work with you..." />
+        <label htmlFor="">Name</label>
+        <input type="text" name="name" value={data.name} onChange={onChange} placeholder="Enter name..." />
+        <label htmlFor="">Email</label>
+        <input type="text" name="email" value={data.email} onChange={onChange} placeholder="Enter email..." />
+        <label htmlFor="">Message</label>
+        <textarea className="textarea" name="message" value={data.message} onChange={onChange} placeholder="I would love to work with you..." />
         <input className="contact-btn" type="submit" value="SUBMIT" />
       </form>
     </div>
