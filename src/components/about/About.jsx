4f36ffaa86a38ca9afe528f6799a4216eca9ce2d
Florian <saubinflorian@gmail.com>
2023-07-21 20:33:09 +0200

    Java & Spring added to about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -17,8 +17,8 @@ export default function About() {
           <i className="devicon-typescript-plain" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="TypeScript"></i>
           <i className="devicon-react-original alternate-react" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="React"></i>
           <i className="devicon-vuejs-plain" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="Vue js"></i>
-          <i className="devicon-python-plain alternate-python" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="Python"></i>
-          <i className="devicon-django-plain" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="Django"></i>
+          <i className="devicon-java-plain alternate-java" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="Java"></i>
+          <i className="devicon-spring-plain" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="Spring"></i>
           <i className="devicon-nodejs-plain alternate-node" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="NodeJS"></i>
           <i className="devicon-mongodb-plain" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="MongoDB"></i>
           <i className="devicon-postgresql-plain alternate-postgre" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="Postgresql"></i>
