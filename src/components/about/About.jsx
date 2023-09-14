560a21a94eb00c93ad4f9f6ef4a0e5b1d2cf25d7
SAUBIN-Florian <saubinflorian@gmail.com>
2023-09-14 14:01:17 +0200

    fullstack now !

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -16,7 +16,7 @@ export default function About() {
         <div className="about-grid">
           <i className="devicon-typescript-plain" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="TypeScript"></i>
           <i className="devicon-react-original alternate-react" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="React"></i>
-          <i className="devicon-vuejs-plain" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="Vue js"></i>
+          <i className="devicon-angularjs-plain" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="Angular"></i>
           <i className="devicon-java-plain alternate-java" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="Java"></i>
           <i className="devicon-spring-plain" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="Spring"></i>
           <i className="devicon-nodejs-plain alternate-node" onMouseOver={handleHover} onMouseLeave={handleLeave} data-name="NodeJS"></i>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -19,7 +19,7 @@ export default function Home() {
         <Fade>
           <div className="hero-banner-content">
             <h1 className="hero-banner-title">Hello, I'm <span className="hero-banner-span">Florian SAUBIN</span> !</h1>
-            <p className="hero-banner-para">I'm a Back-End Developer from Lyon, FR.</p>
+            <p className="hero-banner-para">I'm a Fullstack Developer from Lyon, FR.</p>
             <p className="hero-banner-para">My passion lies in building scalable and efficient back-end systems that power seamless user experiences.</p>
             <p className="hero-banner-para">I enjoy tackling complex challenges and finding innovative solutions to optimize performance and enhance the functionality of web applications.</p>
           </div>
